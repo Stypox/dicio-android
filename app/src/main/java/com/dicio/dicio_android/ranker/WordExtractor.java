0bacdbe7827497cefd7935b685777a029befc23e
Stypox <stypox@pm.me>
2019-09-05 15:49:01 +0200

    Add WordExtractor along with tests

---
+++ app/src/main/java/com/dicio/dicio_android/ranker/WordExtractor.java
@@ -0,0 +1,36 @@
+package com.dicio.dicio_android.ranker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class WordExtractor {
+
+    private static String normalizeWord(String word) {
+        try {
+            if(word.isEmpty()) {
+                return null;
+            }
+
+            // TODO is it ok to use ROOT?
+            return word.toLowerCase(Locale.ROOT);
+        } catch (Throwable ignored) {
+            return null;
+        }
+    }
+
+    public static List<String> extractWords(String input) {
+        // match all non-letter characters
+        String[] splitInput = input.split("[^\\p{L}]+");
+
+        List<String> words = new ArrayList<>();
+        for(String word : splitInput) {
+            String normalized = normalizeWord(word);
+            if (normalized != null) {
+                words.add(normalized);
+            }
+        }
+
+        return words;
+    }
+}
---
+++ app/src/test/java/com/dicio/dicio_android/ranker/WordExtractorTest.java
@@ -0,0 +1,44 @@
+package com.dicio.dicio_android.ranker;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WordExtractorTest {
+    private void assertExtracts(String input, String... expectedWords) {
+        List<String> words = WordExtractor.extractWords(input);
+        assertArrayEquals(words.toString(), expectedWords, words.toArray());
+    }
+
+    @Test
+    public void testDefault() {
+        assertExtracts("\n \n\n\n hello\f   \n  \r  \thow-are  \r   you   \r\n",
+                "hello", "how", "are", "you");
+    }
+
+    @Test
+    public void testCase() {
+        assertExtracts("Hello HOW aRe yoU",
+                "hello", "how", "are", "you");
+    }
+
+    @Test
+    public void testAccent() {
+        assertExtracts("à è ì ò ù À È Ì Ò Ù",
+                "à","è","ì","ò","ù","à","è","ì","ò","ù");
+    }
+
+    @Test
+    public void testSpecialChars() {
+        assertExtracts("btw! hi-hello, how are_ you??? :-) po'",
+                "btw", "hi", "hello", "how", "are", "you", "po");
+    }
+
+    @Test
+    public void testEmpty() {
+        assertExtracts(" \r\n \f\n \r\t\t+-_");
+        assertExtracts("");
+    }
+}
