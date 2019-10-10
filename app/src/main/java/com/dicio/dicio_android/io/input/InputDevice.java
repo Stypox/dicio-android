110262f8d26a8445c5b62dae421c01148abf97b6
Stypox <stypox@pm.me>
2019-10-10 16:07:36 +0200

    Improve comment

--- app/src/main/java/com/dicio/dicio_android/io/input/InputDevice.java
+++ app/src/main/java/com/dicio/dicio_android/io/input/InputDevice.java
@@ -11,18 +11,35 @@ public abstract class InputDevice {
     @Nullable
     private OnInputReceivedListener onInputReceivedListener = null;
 
-    public abstract void startListening();
+    /**
+     * Tries to get input in any way (even asyncronically, if needed).
+     * <br><br>
+     * Overriding functions should report results to the
+     * {@code notifyInputReceived()} and {@code notifyError()} functions.
+     */
+    public abstract void tryToGetInput();
 
     public final void setOnInputReceivedListener(@Nullable OnInputReceivedListener listener) {
         this.onInputReceivedListener = listener;
     }
 
+    /**
+     * This has to be called by functions overriding {@code tryToGetInput()}
+     * when some input from the user is received.
+     * @param input the (raw) received input
+     */
     protected void notifyInputReceived(String input) {
         if (onInputReceivedListener != null) {
             onInputReceivedListener.onInputReceived(input);
         }
     }
 
+    /**
+     * This has to be called by functions overriding {@code tryToGetInput()}
+     * when the user sent some input, but it could not be processed due to
+     * an error.
+     * @param e an exception to handle
+     */
     protected void notifyError(Throwable e) {
         if (onInputReceivedListener != null) {
             onInputReceivedListener.onError(e);
