5d027b0fced9a934a9eef7f48d816b52c9b60082
Stypox <stypox@pm.me>
2020-05-18 15:50:32 +0200

    [TextFallback] Show graphical message

--- app/src/main/java/com/dicio/dicio_android/components/fallback/TextFallbackComponent.java
+++ app/src/main/java/com/dicio/dicio_android/components/fallback/TextFallbackComponent.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.dicio.dicio_android.R;
 import com.dicio.dicio_android.output.graphical.GraphicalOutputDevice;
+import com.dicio.dicio_android.output.graphical.GraphicalOutputUtils;
 import com.dicio.dicio_android.output.speech.SpeechOutputDevice;
 
 import java.util.List;
@@ -22,11 +23,7 @@ public class TextFallbackComponent implements FallbackComponent {
                                GraphicalOutputDevice graphicalOutputDevice) {
 
         speechOutputDevice.speak(context.getString(R.string.eval_speech_no_match));
-
-        /*
-        return new ArrayList<BaseView>() {{
-            add(new Header(context.getString(R.string.eval_header_no_match)));
-        }};
-         */
+        graphicalOutputDevice.display(GraphicalOutputUtils.buildHeader(context,
+                context.getString(R.string.eval_header_no_match)));
     }
 }
