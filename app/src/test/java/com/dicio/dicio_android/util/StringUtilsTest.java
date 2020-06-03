d04b010632394f4c4b0e88be2849ab7f03336556
Stypox <stypox@pm.me>
2020-06-03 15:50:54 +0200

    Add tests for StringUtils join

--- app/src/test/java/com/dicio/dicio_android/util/StringUtilsTest.java
+++ app/src/test/java/com/dicio/dicio_android/util/StringUtilsTest.java
@@ -2,9 +2,28 @@ package com.dicio.dicio_android.util;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.Assert.assertEquals;
 
 public class StringUtilsTest {
+
+    @Test
+    public void defaultJoinTest() {
+        assertEquals("a b c", StringUtils.join(Arrays.asList("a", "b", "c")));
+        assertEquals("", StringUtils.join(Collections.emptyList()));
+        assertEquals("abc", StringUtils.join(Collections.singletonList("abc")));
+        assertEquals(" ", StringUtils.join(Arrays.asList("", "")));
+    }
+
+    @Test
+    public void joinTest() {
+        assertEquals("a-b-c", StringUtils.join("-", Arrays.asList("a", "b", "c")));
+        assertEquals("", StringUtils.join("-", Collections.emptyList()));
+        assertEquals("abc", StringUtils.join("-", Collections.singletonList("abc")));
+        assertEquals("-", StringUtils.join("-", Arrays.asList("", "")));
+    }
 
     @Test
     public void levenshteinDistanceTest() {
@@ -19,13 +38,17 @@ public class StringUtilsTest {
         assertEquals(3, StringUtils.levenshteinDistance("dog", "dosay"));
         assertEquals(1, StringUtils.levenshteinDistance("dog", "doag"));
         assertEquals(1, StringUtils.levenshteinDistance("dog", "dogè"));
+    }
 
-        // uppercase
+    @Test
+    public void levenshteinDistanceCaseTest() {
         assertEquals(3, StringUtils.levenshteinDistance("Kitten", "siTting"));
         assertEquals(0, StringUtils.levenshteinDistance("DOG", "dog"));
         assertEquals(0, StringUtils.levenshteinDistance("cÈd", "CèD"));
+    }
 
-        // other characters
+    @Test
+    public void levenshteinDistanceSpecialTest() {
         assertEquals(0, StringUtils.levenshteinDistance("abc123ABC&%$", "ABC123abc&%$"));
         assertEquals(5, StringUtils.levenshteinDistance("email@email.email", "EMAILatEMAILdotEMAIL"));
     }
