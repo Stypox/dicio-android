9e0cbb126c19e8995d867e4a87e12e687fd1a983
Stypox <stypox@pm.me>
2020-06-05 09:58:53 +0200

    Add urlDecode to ConnectionUtils

--- app/src/main/java/com/dicio/dicio_android/util/ConnectionUtils.java
+++ app/src/main/java/com/dicio/dicio_android/util/ConnectionUtils.java
@@ -1,7 +1,5 @@
 package com.dicio.dicio_android.util;
 
-import android.util.Log;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,23 +7,27 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Scanner;
 
 public class ConnectionUtils {
 
-    public static String getPage(String url) throws IOException {
-        URLConnection connection = new URL(url).openConnection();
-        Scanner scanner = new Scanner(connection.getInputStream());
+    public static String getPage(final String url) throws IOException {
+        final URLConnection connection = new URL(url).openConnection();
+        final Scanner scanner = new Scanner(connection.getInputStream());
         return scanner.useDelimiter("\\A").next();
     }
 
-    public static JSONObject getPageJson(String url) throws IOException, JSONException {
-        Log.e("getpagejson", url);
+    public static JSONObject getPageJson(final String url) throws IOException, JSONException {
         return new JSONObject(getPage(url));
     }
 
-    public static String urlEncode(String s) throws UnsupportedEncodingException {
+    public static String urlEncode(final String s) throws UnsupportedEncodingException {
         return URLEncoder.encode(s, "utf8");
+    }
+
+    public static String urlDecode(final String s) throws UnsupportedEncodingException {
+        return URLDecoder.decode(s, "utf8");
     }
 }
