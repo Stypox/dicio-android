7d5253145d77640c0236951e494ad2e95e1f1da9
Stypox <stypox@pm.me>
2020-12-17 21:23:36 +0100

    [Vosk] Copy model from assets to internal data dir

--- app/src/main/java/org/dicio/dicio_android/input/VoskInputDevice.java
+++ app/src/main/java/org/dicio/dicio_android/input/VoskInputDevice.java
@@ -1,19 +1,25 @@
 package org.dicio.dicio_android.input;
 
 import android.app.Activity;
-import android.util.Log;
+import android.content.res.AssetManager;
 
 import androidx.core.app.ActivityCompat;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.kaldi.Assets;
 import org.kaldi.Model;
 import org.kaldi.RecognitionListener;
 import org.kaldi.SpeechRecognizer;
 import org.kaldi.Vosk;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Queue;
 
 import io.reactivex.Completable;
 import io.reactivex.disposables.Disposable;
@@ -21,6 +27,9 @@ import io.reactivex.disposables.Disposable;
 import static android.Manifest.permission.RECORD_AUDIO;
 
 public class VoskInputDevice extends SpeechInputDevice {
+
+    public static final String MODEL_PATH = "/vosk-model";
+    public static final String ASSETS_SUBPATH = "sync/model-android";
 
     static {
         System.loadLibrary("kaldi_jni");
@@ -37,21 +46,68 @@ public class VoskInputDevice extends SpeechInputDevice {
         ActivityCompat.requestPermissions(activity, new String[]{RECORD_AUDIO}, 5);
     }
 
+    private static void copyFromSyncAssetsToModelFolder(final AssetManager assetManager,
+                                                        final String assetsPath,
+                                                        final File modelPath) throws IOException {
+        final File outFile = new File(modelPath, assetsPath.substring(ASSETS_SUBPATH.length()));
+        outFile.getParentFile().mkdirs();
+
+        try (final InputStream in = assetManager.open(assetsPath);
+             final OutputStream out = new FileOutputStream(outFile)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            out.flush();
+        }
+    }
+
+    private static void copyModelIfNeeded(final File modelPath,
+                                          final AssetManager assetManager) throws IOException {
+        if (!new File(modelPath, "README").exists()) { // check if one file in correct place
+            final Queue<String> paths = new ArrayDeque<>(Collections.singletonList(ASSETS_SUBPATH));
+
+            while (true) {
+                final String path = paths.poll();
+                if (path == null) {
+                    break;
+                }
+
+                final String[] subPaths = assetManager.list(path);
+                if (subPaths.length == 0) {
+                    // this is a file, copy it
+                    if (!path.endsWith(".md5")) { // ignore checksum files
+                        copyFromSyncAssetsToModelFolder(assetManager, path, modelPath);
+                    }
+                } else {
+                    // this is a directory, navigate further
+                    for (final String subPath : subPaths) {
+                        paths.add(path + "/" + subPath);
+                    }
+                }
+            }
+        }
+    }
+
+    synchronized void initializeRecognizer() throws IOException {
+        final File modelPath = new File(activity.getFilesDir(), MODEL_PATH);
+        copyModelIfNeeded(modelPath, activity.getAssets());
+
+        Vosk.SetLogLevel(0);
+        final Model model = new Model(modelPath.getAbsolutePath());
+        recognizer = new SpeechRecognizer(model);
+    }
+
     @Override
     public void startListening() {
         if (recognizer == null) {
-            if (buildDisposable != null) {
-                return;
+            if (buildDisposable == null) {
+                // not already being initialized
+                buildDisposable = Completable.fromAction(this::initializeRecognizer)
+                        .subscribe(this::startListening, Throwable::printStackTrace);
             }
-
-            buildDisposable = Completable.fromAction(() -> {
-                final Assets assets = new Assets(activity);
-                final File assetDir = assets.syncAssets();
-
-                Vosk.SetLogLevel(0);
-                final Model model = new Model(assetDir.toString() + "/model-android");
-                recognizer = new SpeechRecognizer(model);
-            }).subscribe(this::startListening, Throwable::printStackTrace);
             return;
         }
 
