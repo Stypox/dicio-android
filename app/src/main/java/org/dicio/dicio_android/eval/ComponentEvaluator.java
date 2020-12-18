2c38dfc057b2ba451beb33f79324807825c131ad
Stypox <stypox@pm.me>
2020-12-18 19:35:52 +0100

    Update to RxJava3

--- app/src/main/java/org/dicio/dicio_android/eval/ComponentEvaluator.java
+++ app/src/main/java/org/dicio/dicio_android/eval/ComponentEvaluator.java
@@ -21,10 +21,10 @@ import org.dicio.dicio_android.util.ExceptionUtils;
 
 import java.util.List;
 
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ComponentEvaluator {
     private final ComponentRanker componentRanker;
