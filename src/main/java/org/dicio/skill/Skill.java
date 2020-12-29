e34ddc6ca4d1667e16a8f9cc551e3e2df971e183
Stypox <stypox@pm.me>
2020-12-29 14:52:51 +0100

    Add documentation to Skill

--- src/main/java/org/dicio/skill/Skill.java
+++ src/main/java/org/dicio/skill/Skill.java
@@ -11,14 +11,35 @@ import java.util.Locale;
 
 public interface Skill {
 
+    /**
+     * @see InputRecognizer#specificity()
+     */
     InputRecognizer.Specificity specificity();
 
+    /**
+     * @see InputRecognizer#setInput(String, List, List)
+     */
     void setInput(String input, List<String> inputWords, List<String> normalizedWordKeys);
 
+    /**
+     * @see InputRecognizer#score()
+     */
     float score();
 
+    /**
+     * This will be called if this skill was deemed as the best one which could provide output for
+     * what the user requested and it should therefore process the input previously received with
+     * {@link #setInput(String, List, List)}, so that when {@link #generateOutput(Context,
+     * SpeechOutputDevice, GraphicalOutputDevice)} is called everything is ready.
+     * @see IntermediateProcessor#process(Object, Locale)
+     * @param locale the locale to use, useful for example to customize web requests to get the
+     *               correct language or country.
+     */
     void processInput(Locale locale) throws Exception;
 
+    /**
+     * @see OutputGenerator#generate(Object, Context, SpeechOutputDevice, GraphicalOutputDevice)
+     */
     void generateOutput(Context context,
                         SpeechOutputDevice speechOutputDevice,
                         GraphicalOutputDevice graphicalOutputDevice);
@@ -30,6 +51,9 @@ public interface Skill {
      */
     void cleanup();
 
+    /**
+     * @see OutputGenerator#nextSkills()
+     */
     default List<Skill> nextSkills() {
         return Collections.emptyList();
     }
