0803f687c03b7137e043c7973fd53c955ca1c838
Stypox <stypox@pm.me>
2021-12-23 16:51:44 +0100

    [Search] Pass locale to DuckDuckGo

--- app/src/main/java/org/dicio/dicio_android/skills/search/DuckDuckGoProcessor.java
+++ app/src/main/java/org/dicio/dicio_android/skills/search/DuckDuckGoProcessor.java
@@ -2,11 +2,10 @@ package org.dicio.dicio_android.skills.search;
 
 import static org.dicio.dicio_android.Sentences_en.search;
 
-import android.os.Handler;
-import android.os.Looper;
+import androidx.core.os.LocaleListCompat;
 
 import org.dicio.dicio_android.util.ConnectionUtils;
-import org.dicio.dicio_android.util.ShareUtils;
+import org.dicio.dicio_android.util.LocaleUtils;
 import org.dicio.dicio_android.util.StringUtils;
 import org.dicio.skill.SkillContext;
 import org.dicio.skill.chain.IntermediateProcessor;
@@ -17,6 +16,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,6 +25,14 @@ public class DuckDuckGoProcessor
 
     private static final String duckDuckGoSearchUrl = "https://duckduckgo.com/html/?q=";
 
+    private static final List<String> supportedLocales = Arrays.asList("ar-es", "au-en", "at-de",
+            "be-fr", "be-nl", "br-pt", "bg-bg", "ca-en", "ca-fr", "ct-ca", "cl-es", "cn-zh",
+            "co-es", "hr-hr", "cz-cs", "dk-da", "ee-et", "fi-fi", "fr-fr", "de-de", "gr-el",
+            "hk-tz", "hu-hu", "in-en", "id-en", "ie-en", "il-en", "it-it", "jp-jp", "kr-kr",
+            "lv-lv", "lt-lt", "my-en", "mx-es", "nl-nl", "nz-en", "no-no", "pk-en", "pe-es",
+            "ph-en", "pl-pl", "pt-pt", "ro-ro", "ru-ru", "xa-ar", "sg-en", "sk-sk", "sl-sl",
+            "za-en", "es-ca", "es-es", "se-sv", "ch-de", "ch-fr", "tw-tz", "th-en", "tr-tr",
+            "us-en", "us-es", "ua-uk", "uk-en", "vn-en");
 
     @SuppressWarnings("ConstantConditions") // NullPointerExceptions are handled
     @Override
@@ -39,14 +47,24 @@ public class DuckDuckGoProcessor
             return null;
         }
 
+        // find the locale supported by DuckDuckGo that matches the user locale the most
+        LocaleUtils.LocaleResolutionResult resolvedLocale = null;
+        try {
+            resolvedLocale = LocaleUtils.resolveSupportedLocale(
+                    LocaleListCompat.create(context.getLocale()), supportedLocales);
+        } catch (final LocaleUtils.UnsupportedLocaleException ignored) {
+        }
+        final String locale = resolvedLocale == null ? "" : resolvedLocale.supportedLocaleString;
+
+        // make request using headers
         final String html = ConnectionUtils.getPage(
                 duckDuckGoSearchUrl + ConnectionUtils.urlEncode(queryToSearch),
                 new HashMap<String, String>() {{
-                    put("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0");
+                    put("User-Agent",
+                            "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0");
+                    put("Cookie", "kl=" + locale);
                 }}
         );
-        new Handler(Looper.getMainLooper()).post(() ->
-                ShareUtils.copyToClipboard(context.getAndroidContext(), html));
         final Document document = Jsoup.parse(html);
         final Elements elements = document.select("div[class=links_main links_deep result__body]");
 
@@ -64,7 +82,7 @@ public class DuckDuckGoProcessor
                         = element.select("a[class=result__snippet]").first().html();
 
                 result.add(searchResult);
-            } catch (NullPointerException ignored) {
+            } catch (final NullPointerException ignored) {
             }
         }
         return result;
