b20923b9e9d200e0b6b75dd31861c5fec809d8c8
Stypox <stypox@pm.me>
2022-01-14 09:39:28 +0100

    [Vosk] Fix checking whether model already downloaded
    The French model didn't have a README file in the model zip, so better to check for existence of ivector/ directory

--- app/src/main/java/org/dicio/dicio_android/input/VoskInputDevice.java
+++ app/src/main/java/org/dicio/dicio_android/input/VoskInputDevice.java
@@ -109,8 +109,8 @@ public class VoskInputDevice extends SpeechInputDevice {
      */
     private void load(final boolean manual) {
         if (recognizer == null && !currentlyInitializingRecognizer) {
-            if (new File(getModelDirectory(), "README").exists()) {
-                // one file is in the correct place, so everything should be ok
+            if (new File(getModelDirectory(), "ivector").exists()) {
+                // one directory is in the correct place, so everything should be ok
                 Log.d(TAG, "Vosk model in place");
 
                 currentlyInitializingRecognizer = true;
