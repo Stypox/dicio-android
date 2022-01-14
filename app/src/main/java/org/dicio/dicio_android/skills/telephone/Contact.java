4718519820900bb9aad97cc3ca5e6115b00d8a31
Stypox <stypox@pm.me>
2022-01-14 16:46:20 +0100

    [Telephone] Improve and refactor code

--- app/src/main/java/org/dicio/dicio_android/skills/telephone/Contact.java
+++ app/src/main/java/org/dicio/dicio_android/skills/telephone/Contact.java
@@ -1,23 +1,80 @@
 package org.dicio.dicio_android.skills.telephone;
 
-import java.util.Comparator;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.util.Log;
 
-public class Contact implements Comparable {
+import androidx.annotation.NonNull;
 
-    String name;
-    int distance;
-    String id;
-    public Contact(String name, int distance,String id){
-        this.name=name;
-        this.distance=distance;
-        this.id=id;
+import org.dicio.dicio_android.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Contact {
+
+    private static final String TAG = Contact.class.getSimpleName();
+    private static final String NUMBERS_QUERY = ContactsContract.CommonDataKinds.Phone.NUMBER
+            + " IS NOT NULL AND " + ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?";
+
+    private final String name;
+    private final int distance;
+    private final String id;
+
+    public Contact(final String name, final int distance, final String id){
+        this.name = name;
+        this.distance = distance;
+        this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    @NonNull
+    public List<String> getNumbers(final ContentResolver contentResolver) {
+        final List<String> numbers = new ArrayList<>();
+        try (Cursor phoneNumberCursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, NUMBERS_QUERY, new String[]{ id }, null)) {
 
-    @Override
-    public int compareTo(Object o) {
-        return distance-((Contact)o).distance;
+            final int numberColumnIndex = phoneNumberCursor
+                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            while (phoneNumberCursor.moveToNext()) {
+                final String number = phoneNumberCursor.getString(numberColumnIndex);
+                if (!StringUtils.isNullOrEmpty(number)) {
+                    numbers.add(number);
+                }
+            }
+        } catch (final Exception e) {
+            Log.w(TAG, "Could not get numbers for contact " + name, e);
+        }
+        return numbers;
+    }
+
+    public static List<Contact> getSortedContacts(final ContentResolver contentResolver,
+                                                  final String userContactName) {
+        final List<Contact> contacts = new ArrayList<>();
+        try (Cursor contactCursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null)) {
+            final int contactNameColumnIndex
+                    = contactCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+            final int contactIdColumnIndex
+                    = contactCursor.getColumnIndex(ContactsContract.Contacts._ID);
+            while (contactCursor.moveToNext()) {
+                final String name = contactCursor.getString(contactNameColumnIndex);
+                final String id = contactCursor.getString(contactIdColumnIndex);
+                if (name != null) {
+                    final int distance = StringUtils.levenshteinDistance(name, userContactName);
+                    contacts.add(new Contact(name, distance, id));
+                }
+            }
+        }
+
+        //noinspection ComparatorCombinators
+        Collections.sort(contacts, (c1, c2) -> Integer.compare(c1.distance, c2.distance));
+        return contacts;
     }
 }
--- app/src/main/java/org/dicio/dicio_android/skills/telephone/TelephoneOutput.java
+++ app/src/main/java/org/dicio/dicio_android/skills/telephone/TelephoneOutput.java
@@ -1,126 +1,120 @@
 package org.dicio.dicio_android.skills.telephone;
 
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
 import static org.dicio.dicio_android.Sentences_en.telephone;
 
-import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
+
 import org.dicio.dicio_android.R;
 import org.dicio.dicio_android.output.graphical.GraphicalOutputUtils;
-import org.dicio.dicio_android.util.StringUtils;
+import org.dicio.dicio_android.util.PermissionRequesterActivity;
 import org.dicio.skill.SkillContext;
 import org.dicio.skill.chain.OutputGenerator;
 import org.dicio.skill.output.GraphicalOutputDevice;
 import org.dicio.skill.output.SpeechOutputDevice;
 import org.dicio.skill.standard.StandardResult;
-import android.net.Uri;
-import android.provider.ContactsContract.Contacts;
-import android.provider.ContactsContract;
-import android.database.Cursor;
-import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class TelephoneOutput implements OutputGenerator<StandardResult>{
-    @SuppressLint("Range")
+import java.util.List;
+
+public class TelephoneOutput implements OutputGenerator<StandardResult> {
+
     public void generate(final StandardResult data,
                          final SkillContext context,
                          final SpeechOutputDevice speechOutputDevice,
                          final GraphicalOutputDevice graphicalOutputDevice) {
 
+        final Context androidContext = context.getAndroidContext();
+        if (checkSelfPermission(androidContext, READ_CONTACTS) == PERMISSION_GRANTED
+                && checkSelfPermission(androidContext, CALL_PHONE) == PERMISSION_GRANTED) {
+            onPermissionsGranted(data, context, speechOutputDevice, graphicalOutputDevice);
 
-        final String userContactName = data.getCapturingGroup(telephone.who).trim();
-        if (ContextCompat.checkSelfPermission(context.getAndroidContext(), "android.permission.READ_CONTACTS") == PackageManager.PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(context.getAndroidContext(), "android.permission.CALL_PHONE") == PackageManager.PERMISSION_GRANTED) {
-            ContentResolver cr = context.getAndroidContext().getContentResolver();
-            Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-            ArrayList<Contact> names = new ArrayList<>();
-            while (cur.moveToNext()) {
-
-                String name = cur.getString(cur.getColumnIndex(Contacts.DISPLAY_NAME));
-                String id = cur.getString(cur.getColumnIndex(Contacts._ID));
-                if (name != null) {
-                    int distance = StringUtils.levenshteinDistance(name, userContactName);
-                    names.add(new Contact(name, distance, id));
-                }
-            }
-            cur.close();
-            Collections.sort(names);
-            int count = 0;
-
-            if (names.isEmpty())
-                Log.d("Telephone", "No matching contact found.");
-            else {
-                final LinearLayout output
-                        = GraphicalOutputUtils.buildVerticalLinearLayout(context.getAndroidContext(),
-                        ResourcesCompat.getDrawable(context.getAndroidContext().getResources(),
-                                R.drawable.divider_items, null));
-                for (int z = 0; z < 5 && z < names.size(); z++) {
-                    Contact name = names.get(z);
-                    final View view = GraphicalOutputUtils.inflate(context.getAndroidContext(),
-                            R.layout.skill_telephone_contactlist);
-
-                    ((TextView) view.findViewById(R.id.title))
-                            .setText(name.name);
-                    Log.d("ID_CONTACT", name.id + name.name);
-                    Cursor cur2 = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL AND " + ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{name.id}, null);
-                    try {
-                        while (cur2.moveToNext()) {
-                            String num = cur2.getString(cur2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            if (cur2.getPosition() != 0) {
-                                count++;
-                                final View ViewMoreNumbers = GraphicalOutputUtils.inflate(context.getAndroidContext(),
-                                        R.layout.skill_telephone_contact_second_number);
-                                ((TextView) ViewMoreNumbers.findViewById(R.id.description))
-                                        .setText(num);
-                                ViewMoreNumbers.setOnClickListener(v -> call(num, context.getAndroidContext()));
-                                output.addView(ViewMoreNumbers);
-                            } else {
-
-                                ((TextView) view.findViewById(R.id.description))
-                                        .setText(num);
-                                view.setOnClickListener(v -> call(num, context.getAndroidContext()));
-                                output.addView(view);
-                            }
-
-
-                        }
-                        cur2.close();
-                    } catch (Exception E) {
-                        E.printStackTrace();
-                    }
-                }
-                if (count != 0) {
-                    speechOutputDevice.speak(context.getAndroidContext().getString(R.string.skill_telephone_found_contacts, count + ""));
-                    graphicalOutputDevice.display(output);
+        } else {
+            PermissionRequesterActivity.requestPermissions(androidContext,
+                    new String[]{READ_CONTACTS, CALL_PHONE}, granted -> {
+                if (granted) {
+                    onPermissionsGranted(data, context, speechOutputDevice, graphicalOutputDevice);
                 } else {
-                    speechOutputDevice.speak(context.getAndroidContext().getString(R.string.skill_telephone_unknown_contact));
+                    final String message
+                            = androidContext.getString(R.string.skill_telephone_missing_permission);
+                    speechOutputDevice.speak(message);
+                    graphicalOutputDevice.display(GraphicalOutputUtils.buildSubHeader(
+                            androidContext, message));
                 }
-            }
-
-
+            });
         }
     }
 
-
-
-    public void call(String tel, Context context) {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:"+tel));
-        context.startActivity(callIntent);
-    }
-
-
-
-
     @Override
     public void cleanup() {
+    }
+
+    private void onPermissionsGranted(final StandardResult data,
+                                      final SkillContext context,
+                                      final SpeechOutputDevice speechOutputDevice,
+                                      final GraphicalOutputDevice graphicalOutputDevice) {
+        final String userContactName = data.getCapturingGroup(telephone.who).trim();
+        final ContentResolver contentResolver = context.getAndroidContext().getContentResolver();
+        final List<Contact> contacts = Contact.getSortedContacts(contentResolver, userContactName);
+
+        LinearLayout output = null;
+        int contactCount = 0;
+        if (!contacts.isEmpty()) {
+            output = GraphicalOutputUtils.buildVerticalLinearLayout(context.getAndroidContext(),
+                    ResourcesCompat.getDrawable(context.getAndroidContext().getResources(),
+                            R.drawable.divider_items, null));
+
+            for (int i = 0; i < 5 && i < contacts.size(); ++i) {
+                final Contact contact = contacts.get(i);
+                final List<String> numbers = contact.getNumbers(contentResolver);
+                if (numbers.isEmpty()) {
+                    continue;
+                }
+                ++contactCount;
+
+                for (int j = 0; j < contacts.size(); ++j) {
+                    final View view;
+                    if (j == 0) {
+                        view = GraphicalOutputUtils.inflate(context.getAndroidContext(),
+                                R.layout.skill_telephone_contactlist);
+                        ((TextView) view.findViewById(R.id.title)).setText(contact.getName());
+                    } else {
+                        // a contact can have multiple associated numbers
+                        view = GraphicalOutputUtils.inflate(context.getAndroidContext(),
+                                R.layout.skill_telephone_contact_second_number);
+                    }
+
+                    final String number = numbers.get(i);
+                    ((TextView) view.findViewById(R.id.description)).setText(number);
+                    view.setOnClickListener(v -> call(context.getAndroidContext(), number));
+                    output.addView(view);
+                }
+            }
+        }
+
+        if (contactCount == 0) {
+            speechOutputDevice.speak(context.getAndroidContext().getString(
+                    R.string.skill_telephone_unknown_contact));
+        } else {
+            speechOutputDevice.speak(context.getAndroidContext().getString(
+                    R.string.skill_telephone_found_contacts, contactCount));
+            graphicalOutputDevice.display(output);
+        }
+    }
+
+    private static void call(final Context context, final String number) {
+        final Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + number));
+        context.startActivity(callIntent);
     }
 }
