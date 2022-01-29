df11df55eaf8b3ecf2cff27af012a345abce5b30
Stypox <stypox@pm.me>
2022-01-29 10:11:59 +0100

    Add skill test

---
+++ src/test/java/org/dicio/skill/SkillTest.java
@@ -0,0 +1,39 @@
+package org.dicio.skill;
+
+import static org.junit.Assert.assertSame;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.dicio.skill.chain.InputRecognizer;
+import org.dicio.skill.output.GraphicalOutputDevice;
+import org.dicio.skill.output.SpeechOutputDevice;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SkillTest {
+
+    @Test
+    public void testConstructorAndGetSkillInfo() {
+        final SkillInfo skillInfo = new SkillInfo("id", 0, 0, 0, false) {
+            @Override public boolean isAvailable(final SkillContext context) { return false; }
+            @Override public Skill build(final SkillContext context) { return null; }
+            @Nullable @Override public Fragment getPreferenceFragment() { return null; }
+        };
+
+        final Skill skill = new Skill(skillInfo) {
+            @Override public InputRecognizer.Specificity specificity() { return null; }
+            @Override public void setInput(final String input, final List<String> inputWords,
+                final List<String> normalizedWordKeys) {}
+            @Override public float score() { return 0; }
+            @Override public void processInput(final SkillContext context) {}
+            @Override public void generateOutput(final SkillContext context,
+                    final SpeechOutputDevice speechOutputDevice,
+                    final GraphicalOutputDevice graphicalOutputDevice) {}
+            @Override public void cleanup() {}
+        };
+
+        assertSame(skillInfo, skill.getSkillInfo());
+    }
+}
