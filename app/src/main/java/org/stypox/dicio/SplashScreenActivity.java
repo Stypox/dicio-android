12a66e9fa58b0ead034e00893a9964aabd4eacc2
Ankit Sharma <ankit.sharma722327@gmail.com>
2023-01-05 14:20:10 +0530

    Add Animated Splash Screen which enhances the User Interface of the Android App.

---
+++ app/src/main/java/org/stypox/dicio/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package org.stypox.dicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import org.stypox.dicio.databinding.ActivitySplashScreenBinding;
+import org.stypox.dicio.skills.timer.TimerOutput;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ActivitySplashScreenBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
+
+    }
+}
