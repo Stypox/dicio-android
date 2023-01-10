d3db45777b62e7160d0beee38346d36144114181
Stypox <stypox@pm.me>
2023-01-10 19:03:32 +0100

    [Navigation] Prefer ordinals, use String instead of Data,
    implement next skills

--- app/src/main/java/org/dicio/dicio_android/skills/navigation/NavigationOutput.java
+++ app/src/main/java/org/dicio/dicio_android/skills/navigation/NavigationOutput.java
@@ -1,45 +1,102 @@
 package org.dicio.dicio_android.skills.navigation;
 
+import static org.dicio.dicio_android.Sections.getSection;
+
 import android.content.Intent;
 import android.net.Uri;
 
-import org.dicio.dicio_android.R;
-import org.dicio.dicio_android.output.graphical.GraphicalOutputUtils;
-import org.dicio.skill.chain.OutputGenerator;
+import androidx.annotation.Nullable;
 
+import org.dicio.dicio_android.R;
+import org.dicio.dicio_android.SectionsGenerated;
+import org.dicio.dicio_android.output.graphical.GraphicalOutputUtils;
+import org.dicio.dicio_android.util.StringUtils;
+import org.dicio.skill.Skill;
+import org.dicio.skill.chain.ChainSkill;
+import org.dicio.skill.chain.InputRecognizer;
+import org.dicio.skill.chain.OutputGenerator;
+import org.dicio.skill.standard.StandardRecognizer;
+import org.dicio.skill.standard.StandardResult;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
-public class NavigationOutput extends OutputGenerator<NavigationOutput.Data> {
+public class NavigationOutput extends OutputGenerator<String> {
 
-    public static class Data {
-        public boolean failed = false;
-        public String address;
-    }
+    private boolean tryAgain = false;
 
     @Override
-    public void generate(final Data data) {
-        if (data.failed) {
+    public void generate(@Nullable final String data) {
+        if (StringUtils.isNullOrEmpty(data)) {
             final String msg = ctx().android().getString(R.string.skill_navigation_specify_where);
             ctx().getSpeechOutputDevice().speak(msg);
             ctx().getGraphicalOutputDevice().display(GraphicalOutputUtils.buildSubHeader(
                     ctx().android(), msg));
+            tryAgain = true;
             return;
         }
+        tryAgain = false;
 
-        final String message = ctx().android().getString(R.string.skill_navigation_navigating_to,
-                data.address);
-
-        ctx().getSpeechOutputDevice().speak(message);
+        final String msg = ctx().android().getString(R.string.skill_navigation_navigating_to, data);
+        ctx().getSpeechOutputDevice().speak(msg);
         ctx().getGraphicalOutputDevice().display(GraphicalOutputUtils.buildSubHeader(
-                ctx().android(), message));
+                ctx().android(), msg));
 
-        final String uriGeoSimple = String.format(Locale.ENGLISH,
-                "geo:0,0?q=%s", data.address);
-
-        final Intent launchIntent = new Intent(Intent.ACTION_VIEW,
-                Uri.parse(uriGeoSimple));
+        final String uriGeoSimple = String.format(Locale.ENGLISH, "geo:0,0?q=%s", data);
+        final Intent launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriGeoSimple));
         ctx().android().startActivity(launchIntent);
+    }
 
+    @Override
+    public List<Skill> nextSkills() {
+        if (!tryAgain) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.asList(
+                new ChainSkill.Builder()
+                        .recognize(new StandardRecognizer(getSection(SectionsGenerated.navigation)))
+                        .process(new NavigationProcessor())
+                        .output(new NavigationOutput()),
+                new ChainSkill.Builder()
+                        .recognize(new InputRecognizer<StandardResult>() {
+                            private String input;
+
+                            @Override
+                            public Specificity specificity() {
+                                return Specificity.low;
+                            }
+
+                            @Override
+                            public void setInput(final String input,
+                                                 final List<String> inputWords,
+                                                 final List<String> normalizedInputWords) {
+                                this.input = input;
+                            }
+
+                            @Override
+                            public float score() {
+                                return 1.0f;
+                            }
+
+                            @Override
+                            public StandardResult getResult() {
+                                return new StandardResult("", input, null) {
+                                    @Override
+                                    public String getCapturingGroup(final String name) {
+                                        return input;
+                                    }
+                                };
+                            }
+
+                            @Override
+                            public void cleanup() {
+                            }
+                        })
+                        .process(new NavigationProcessor())
+                        .output(new NavigationOutput()));
     }
 
     @Override
--- app/src/main/java/org/dicio/dicio_android/skills/navigation/NavigationProcessor.java
+++ app/src/main/java/org/dicio/dicio_android/skills/navigation/NavigationProcessor.java
@@ -2,62 +2,64 @@ package org.dicio.dicio_android.skills.navigation;
 
 import static org.dicio.dicio_android.Sentences_en.navigation;
 
-import org.dicio.dicio_android.util.StringUtils;
+import org.dicio.numbers.NumberParserFormatter;
+import org.dicio.numbers.util.Number;
 import org.dicio.skill.chain.IntermediateProcessor;
 import org.dicio.skill.standard.StandardResult;
 
 import java.util.List;
 
-public class NavigationProcessor
-        extends IntermediateProcessor<StandardResult, NavigationOutput.Data> {
+public class NavigationProcessor extends IntermediateProcessor<StandardResult, String> {
     @Override
-    public NavigationOutput.Data process(final StandardResult data) throws Exception {
-        final NavigationOutput.Data result = new NavigationOutput.Data();
-
+    public String process(final StandardResult data) throws Exception {
         if (data == null) {
-            result.failed = true;
-            return result;
+            return null;
         }
 
-        String placeToNavigate = data.getCapturingGroup(navigation.where);
-
+        final String placeToNavigate = data.getCapturingGroup(navigation.where);
         if (placeToNavigate == null) {
-            result.failed = true;
-            return result;
+            return null;
         }
 
-        placeToNavigate = placeToNavigate.trim();
+        final NumberParserFormatter npf = ctx().getNumberParserFormatter();
+        if (npf == null) {
+            // No number parser available, feed the spoken input directly to the map application.
+            return placeToNavigate.trim();
 
-        if (StringUtils.isNullOrEmpty(placeToNavigate)) {
-            result.failed = true;
-            return result;
+        } else {
+            final List<Object> textWithNumbers = ctx().requireNumberParserFormatter()
+                    .extractNumbers(data.getCapturingGroup(navigation.where))
+                    .preferOrdinal(true)
+                    .get();
+
+            // Given an address of "9546 19 avenue", the building number is 9546 and the street
+            // number is 19.
+            //
+            // Known issues:
+            // - Saying the building number using its digits one by one results in undesired spaces
+            //   in between each one
+            // - Saying the building number using partial grouping of its digits (but not all of
+            //   them) e.g. "ninety five forty six" also results in undesired spaces in between each
+            //   partial grouping
+            //
+            // Based on these known issues, for the example address given above, the speech provided
+            // by the user should be "nine thousand five hundred forty six nineteen(th) avenue".
+
+            final StringBuilder placeToNavigateSB = new StringBuilder();
+            for (final Object currentItem : textWithNumbers) {
+                if (currentItem instanceof String) {
+                    placeToNavigateSB.append(currentItem);
+                } else if (currentItem instanceof Number) {
+                    final Number number = (Number) currentItem;
+                    if (number.isInteger()) {
+                        placeToNavigateSB.append(number.integerValue());
+                    } else {
+                        placeToNavigateSB.append(number.decimalValue());
+                    }
+                }
+            }
+
+            return placeToNavigateSB.toString().trim();
         }
-
-        final List<Object> textWithNumbers = ctx().requireNumberParserFormatter()
-                .extractNumbers(data.getCapturingGroup(navigation.where)).get();
-
-        // Given an address of "9546 19 avenue", the building number is 9546 and the street number
-        // is 19.
-        //
-        // Known issues:
-        // - Specifying the street number as an ordinal seems to result in some undesired
-        // multiplication happening
-        // - Saying the building number using its digits one by one results in undesired spaces in
-        // between each one
-        // - Saying the building number using partial grouping of its digits (but not all of them)
-        // e.g. "ninety five forty six" also results in undesired spaces in between each partial
-        // grouping
-        //
-        // Based on these known issues, for the example address given above, the speech provided by
-        // the user should be "nine thousand five hundred forty six nineteen avenue".
-
-        final StringBuilder placeToNavigateSB = new StringBuilder();
-        for (final Object currentItem : textWithNumbers) {
-            placeToNavigateSB.append(currentItem.toString());
-        }
-
-        result.failed = false;
-        result.address = placeToNavigateSB.toString().trim();
-        return result;
     }
 }
