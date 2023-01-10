505925cedb5ed32f5b91d47227b0af91ceb2df9e
Stypox <stypox@pm.me>
2023-01-10 21:41:01 +0100

    Add setNextSkills to OutputGenerator

--- src/main/java/org/dicio/skill/chain/OutputGenerator.java
+++ src/main/java/org/dicio/skill/chain/OutputGenerator.java
@@ -1,5 +1,7 @@
 package org.dicio.skill.chain;
 
+import androidx.annotation.CallSuper;
+
 import org.dicio.skill.Skill;
 import org.dicio.skill.SkillComponent;
 import org.dicio.skill.util.CleanableUp;
@@ -15,6 +17,9 @@ import java.util.List;
  */
 public abstract class OutputGenerator<FromType> extends SkillComponent implements CleanableUp {
 
+    // no next skills by default
+    private List<Skill> nextSkills = Collections.emptyList();
+
     /**
      * Generates output to speak or to show to the user based on the data calculated in the previous
      * steps. Do here calculations that require access to platform-specific APIs, such as looking
@@ -26,16 +31,41 @@ public abstract class OutputGenerator<FromType> extends SkillComponent implement
 
     /**
      * @return a list of skills to use for the next user input. This is needed to allow providing a
-     *         stateful interaction with a set of skills. If the list is empty, and it is by
-     *         default, the current stateful conversation is interrupted. This function will be
-     *         called after {@link #generate(Object)}, so that the calculated data can be used to
-     *         choose what to do. There is no need to call {@link
-     *         Skill#setContext(org.dicio.skill.SkillContext)} and {@link
-     *         Skill#setSkillInfo(org.dicio.skill.SkillInfo)} on the returned skills, as that has to
-     *         be done by the caller.
+     *         stateful interaction with a set of skills. If the list is empty, the current stateful
+     *         conversation is interrupted. This function will be called only once, after {@link
+     *         #generate(Object)}, so that the calculated data can be used to choose what to do.
+     *         There is no need to call {@link Skill#setContext(org.dicio.skill.SkillContext)} and
+     *         {@link Skill#setSkillInfo(org.dicio.skill.SkillInfo)} on the returned skills, as that
+     *         has to be done by the caller. By default this method returns the last list fed to
+     *         {@link #setNextSkills(List)} during the last {@link #generate(Object)} phase (and
+     *         then resets that list to an empty list), or an empty list if no list was fed.
      */
     public List<Skill> nextSkills() {
-        // no next skills by default
-        return Collections.emptyList();
+        final List<Skill> skills = nextSkills;
+        nextSkills = Collections.emptyList();
+        return skills;
+    }
+
+    /**
+     * @param skills the list of skills to return on the next call to {@link #nextSkills()}. Clears
+     *               any previously set list of skills. There is no need to call {@link
+     *               Skill#setContext(org.dicio.skill.SkillContext)} and {@link
+     *               Skill#setSkillInfo(org.dicio.skill.SkillInfo)} on the skills, as that has to be
+     *               done by the caller of {@link #nextSkills()}.
+     */
+    protected void setNextSkills(final List<Skill> skills) {
+        nextSkills = skills;
+    }
+
+    /**
+     * Resets the last list of next skills passed to {@link #setNextSkills(List)} to an empty list.
+     * Override to stop anything this object is doing, dispose disposables, detach listeners, set
+     * references to external objects to {@code null}, release resources, etc... Remember to call
+     * super if you override.
+     */
+    @Override
+    @CallSuper
+    public void cleanup() {
+        nextSkills = Collections.emptyList();
     }
 }
--- src/test/java/org/dicio/skill/SkillTest.java
+++ src/test/java/org/dicio/skill/SkillTest.java
@@ -1,10 +1,6 @@
 package org.dicio.skill;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import static org.junit.Assert.assertTrue;
 
 import org.dicio.skill.chain.InputRecognizer;
 import org.junit.Test;
@@ -13,16 +9,22 @@ import java.util.List;
 
 public class SkillTest {
 
-    @Test
-    public void testConstructor() {
-        new Skill() {
+    public static Skill buildEmptySkill() {
+        return new Skill() {
             @Override public InputRecognizer.Specificity specificity() { return null; }
             @Override public void setInput(final String input, final List<String> inputWords,
-                    final List<String> normalizedWordKeys) {}
+                                           final List<String> normalizedWordKeys) {}
             @Override public float score() { return 0; }
             @Override public void processInput() {}
             @Override public void generateOutput() {}
             @Override public void cleanup() {}
         };
+    }
+
+    @Test
+    public void testConstructor() {
+        final Skill skill = buildEmptySkill();
+
+        assertTrue(skill.nextSkills().isEmpty());
     }
 }
