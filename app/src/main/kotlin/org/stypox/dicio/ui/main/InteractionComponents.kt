5758008e641f4bb208b7e142ef85fa3009ddafee
Stypox <stypox@pm.me>
2024-05-14 23:31:17 +0200

    Add skill icon next to loading answer card

--- app/src/main/kotlin/org/stypox/dicio/ui/main/InteractionComponents.kt
+++ app/src/main/kotlin/org/stypox/dicio/ui/main/InteractionComponents.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -16,12 +18,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -117,12 +121,20 @@ fun ConfirmedQuestionCard(@PreviewParameter(UserInputPreviews::class) userInput:
 @Composable
 fun LoadingAnswerCard(@PreviewParameter(SkillInfoPreviews::class) skill: SkillInfo) {
     MessageCard(containerColor = MaterialTheme.colorScheme.secondaryContainer) {
-        Box(
-            contentAlignment = Alignment.Center,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            Icon(
+                painter = painterResource(id = skill.iconResource),
+                contentDescription = null,
+                tint = ProgressIndicatorDefaults.circularColor,
+                modifier = Modifier.size(40.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
             CircularProgressIndicator()
         }
     }
