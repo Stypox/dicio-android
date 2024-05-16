6a5caba23d7f99651462198f9e1fea1fb08468ef
Stypox <stypox@pm.me>
2024-05-16 13:15:29 +0200

    Improve MissingPermissionsSkillOutput

--- app/src/main/kotlin/org/stypox/dicio/io/graphical/MissingPermissionsSkillOutput.kt
+++ app/src/main/kotlin/org/stypox/dicio/io/graphical/MissingPermissionsSkillOutput.kt
@@ -1,7 +1,18 @@
 package org.stypox.dicio.io.graphical
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.dicio.skill.SkillContext
 import org.dicio.skill.SkillInfo
 import org.dicio.skill.output.SkillOutput
@@ -21,6 +32,21 @@ class MissingPermissionsSkillOutput(
 
     @Composable
     override fun GraphicalOutput(ctx: SkillContext) {
-        Text(text = getSpeechOutput(ctx))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                painter = painterResource(id = skill.iconResource),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(40.dp)
+            )
+            Text(
+                text = getSpeechOutput(ctx),
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
