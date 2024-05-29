1b19332c7995d39cb6745de12a03aa0577321230
Stypox <stypox@pm.me>
2024-05-29 11:33:14 +0200

    Simplify OptionalComponent

--- skill/src/main/java/org/dicio/skill/standard2/component/OptionalComponent.kt
+++ skill/src/main/java/org/dicio/skill/standard2/component/OptionalComponent.kt
@@ -3,13 +3,8 @@ package org.dicio.skill.standard2.component
 import org.dicio.skill.standard2.StandardMatchResult
 import org.dicio.skill.standard2.helper.MatchHelper
 
-data class OptionalComponent(
-    val component: Component
-) : Component {
+object OptionalComponent : Component {
     override fun match(start: Int, end: Int, ctx: MatchHelper): StandardMatchResult {
-        return StandardMatchResult.keepBest(
-            StandardMatchResult.empty(start, false),
-            component.match(start, end, ctx)
-        )
+        return StandardMatchResult.empty(start, false)
     }
 }
