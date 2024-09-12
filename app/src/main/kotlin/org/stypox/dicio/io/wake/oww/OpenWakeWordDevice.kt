87d118c0894b2d39c423be9868927c5183b8f4ae
Stypox <stypox@pm.me>
2024-09-12 22:04:05 +0200

    Add some flows

--- app/src/main/kotlin/org/stypox/dicio/io/wake/oww/OpenWakeWordDevice.kt
+++ app/src/main/kotlin/org/stypox/dicio/io/wake/oww/OpenWakeWordDevice.kt
@@ -1,15 +1,18 @@
 package org.stypox.dicio.io.wake.oww
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.stypox.dicio.io.wake.WakeDevice
 import org.stypox.dicio.io.wake.WakeState
 
 class OpenWakeWordDevice : WakeDevice {
-    override val state: StateFlow<WakeState>
-        get() = TODO("Not yet implemented")
-    override val wakeWordTriggered: Flow<Unit>
-        get() = TODO("Not yet implemented")
+    private val _state: MutableStateFlow<WakeState> = MutableStateFlow(WakeState.Loading)
+    override val state: StateFlow<WakeState> = _state
+
+    private val _wakeWordTriggered: MutableSharedFlow<Unit> = MutableSharedFlow(replay = 0)
+    override val wakeWordTriggered: Flow<Unit> = _wakeWordTriggered
 
     override fun download() {
         TODO("Not yet implemented")
