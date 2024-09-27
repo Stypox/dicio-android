ed2ab74bcc522a5a3a8a8e150ef3f004cebbaf2d
Stypox <stypox@pm.me>
2024-09-28 00:46:46 +0200

    Also request USE_FULL_SCREEN_INTENT perm

--- app/src/main/kotlin/org/stypox/dicio/ui/home/WakeWordWidget.kt
+++ app/src/main/kotlin/org/stypox/dicio/ui/home/WakeWordWidget.kt
@@ -35,7 +35,10 @@ import org.stypox.dicio.ui.util.LoadingProgress
 import org.stypox.dicio.ui.util.WakeStatesPreviews
 import org.stypox.dicio.ui.util.loadingProgressString
 
-val wakeWordPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+val wakeWordPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.POST_NOTIFICATIONS,
+            Manifest.permission.USE_FULL_SCREEN_INTENT)
+else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
     arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.POST_NOTIFICATIONS)
 else
     arrayOf(Manifest.permission.RECORD_AUDIO)
