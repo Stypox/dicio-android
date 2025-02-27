094f88ada673677a6eef66b01e83234b09826523
Stypox <stypox@pm.me>
2025-02-28 00:02:04 +0100

    Fix tests

--- app/src/androidTest/kotlin/org/stypox/dicio/screenshot/FakeWakeDeviceWrapper.kt
+++ app/src/androidTest/kotlin/org/stypox/dicio/screenshot/FakeWakeDeviceWrapper.kt
@@ -7,12 +7,10 @@ import org.stypox.dicio.io.wake.WakeState
 
 class FakeWakeDeviceWrapper : WakeDeviceWrapper {
     override val state: StateFlow<WakeState?> = MutableStateFlow(null)
+    override val isHeyDicio: StateFlow<Boolean> = MutableStateFlow(true)
 
     override fun download() {}
-
     override fun processFrame(audio16bitPcm: ShortArray): Boolean = false
-
     override fun frameSize(): Int = 1312
-
     override fun reinitializeToReleaseResources() {}
 }
