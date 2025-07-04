7bfac48d5302981abd2530c350b05d1d48ba6945
Stypox <stypox@pm.me>
2025-07-04 14:47:06 +0200

    Release v3.2 (16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "org.stypox.dicio"
         minSdk = 21
         targetSdk = 36
-        versionCode = 15
-        versionName = "3.1"
+        versionCode = 16
+        versionName = "3.2"
         testInstrumentationRunner = "org.stypox.dicio.CustomTestRunner"
 
         vectorDrawables.useSupportLibrary = true
