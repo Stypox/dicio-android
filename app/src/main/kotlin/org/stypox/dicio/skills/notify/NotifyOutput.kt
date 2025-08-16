0eb5e9e6bc058a72ce6ab458a19051d59a2e11d9
Billythegreat <billythegreat@duck.com>
2025-08-16 14:25:13 -0400

    Everything except the NotifySkill.kt

--- app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyOutput.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyOutput.kt
@@ -1,4 +1,19 @@
 package org.stypox.dicio.skills.notify
 
-class NotifyOutput {
+import androidx.compose.runtime.Composable
+import org.dicio.skill.context.SkillContext
+import org.dicio.skill.skill.SkillOutput
+import org.stypox.dicio.R
+import org.stypox.dicio.util.getString
+
+data class NotifyOutput(val message: String, val appName: String): SkillOutput {
+    override fun getSpeechOutput(ctx: SkillContext): String {
+        return ctx.getString(R.string.skill_notify_message, appName)
+
+    }
+
+    @Composable
+    override fun GraphicalOutput(ctx: SkillContext) {
+        TODO("Not yet implemented")
+    }
 }
