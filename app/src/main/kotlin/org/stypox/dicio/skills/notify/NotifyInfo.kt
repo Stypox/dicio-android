a8b0a369a332f8cf37d416b80b1409ccb796cd18
Billythegreat <billythegreat@duck.com>
2025-08-23 19:34:47 -0400

    Notify fix string & calculator error

--- app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyInfo.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyInfo.kt
@@ -1,8 +1,8 @@
 package org.stypox.dicio.skills.notify
 
-import android.Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE
 import android.content.Context
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NotificationsActive
 import androidx.compose.material.icons.filled.Watch
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -12,19 +12,18 @@ import org.dicio.skill.skill.Skill
 import org.dicio.skill.skill.SkillInfo
 import org.stypox.dicio.R
 import org.stypox.dicio.sentences.Sentences
-import org.stypox.dicio.util.PERMISSION_CALL_PHONE
-import org.stypox.dicio.util.PERMISSION_READ_CONTACTS
+import org.stypox.dicio.util.PERMISSION_NOTIFICATION_LISTENER
 
 object NotifyInfo: SkillInfo("notify") {
     override fun name(context: Context) =
-        R.string.skill_name_notify.toString()
+        context.getString(R.string.skill_name_notify)
 
     override fun sentenceExample(context: Context): String =
-        R.string.skill_sentence_example_notify.toString()
+        context.getString(R.string.skill_sentence_example_notify)
 
     @Composable
     override fun icon() =
-        rememberVectorPainter(Icons.Default.Watch)
+        rememberVectorPainter(Icons.Default.NotificationsActive)
 
     override fun isAvailable(ctx: SkillContext): Boolean {
         return Sentences.Notify[ctx.sentencesLanguage] != null
@@ -32,7 +31,7 @@ object NotifyInfo: SkillInfo("notify") {
     }
 
     override val neededPermissions: List<Permission>
-            = listOf()
+            = listOf(PERMISSION_NOTIFICATION_LISTENER)
 
     override fun build(ctx: SkillContext): Skill<*> {
         return NotifySkill(NotifyInfo, Sentences.Notify[ctx.sentencesLanguage]!!)
