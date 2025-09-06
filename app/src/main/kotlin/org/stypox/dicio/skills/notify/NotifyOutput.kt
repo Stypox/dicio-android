5b096376d6a1b43ef89d8445323145bc847c823b
billy-thegreat <billyjasinski85@gmail.com>
2025-09-06 13:10:04 -0400

    Notify fix null notifications

--- app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyOutput.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/notify/NotifyOutput.kt
@@ -1,6 +1,5 @@
 package org.stypox.dicio.skills.notify
 
-import android.util.Log // Import Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -9,17 +8,13 @@ import org.dicio.skill.context.SkillContext
 import org.dicio.skill.skill.SkillOutput
 import org.stypox.dicio.R
 import org.stypox.dicio.util.getString
-import org.stypox.dicio.skills.notify.NotifyHandler.Companion.TAG
 
 data class NotifyOutput(val notifications: List<Notification>): SkillOutput {
     override fun getSpeechOutput(ctx: SkillContext): String {
-        Log.d(TAG, "getSpeechOutput called")
         var response = ""
         if (notifications.isNotEmpty()) {
             notifications.forEachIndexed { index, notification ->
-                Log.d(TAG, "index=$index appName='${notification.appName}', message='${notification.message}'")
                 val speechPart = ctx.getString(R.string.skill_notify_message, notification.appName, notification.message ?: "")
-                Log.d(TAG, "Speech part for notification $index: '$speechPart'")
                 response += speechPart
                 if (notifications.size > 1 && index < notifications.size - 1) {
                     response += ". "
@@ -28,10 +23,7 @@ data class NotifyOutput(val notifications: List<Notification>): SkillOutput {
         }
         else {
             response = ctx.getString(R.string.no_notifications)
-            Log.d(TAG, response)
         }
-        Log.d(TAG, response)
-        Log.e(TAG, "notifications: " + notifications.toString())
         return response.trim()
     }
 
